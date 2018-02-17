b2e3735682798c02bb5baafc03bee141d800dc92
Abhi Gupta <abhigupta99@icloud.com>
2018-02-16 22:38:03 -0500

    update

--- Python/facebook.py
+++ Python/facebook.py
@@ -2,4 +2,29 @@
 # @Author: Abhi
 # @Date:   2018-02-16 21:04:55
 # @Last Modified by:   Abhi
-# @Last Modified time: 2018-02-16 21:04:55
+# @Last Modified time: 2018-02-16 22:34:28
+
+from fbchat import Client
+from fbchat.models import *
+
+class Facebook:
+	def __init__(self, email, password):
+		self.user = Client(email, password)
+		self.name = self.user.name
+		self.id = self.user.uid
+
+	def sendMessage(self, recipient, message, threadType = ThreadType.GROUP):
+		self.user.send(Message(text=message, thread_id=self.getThreadID(recipient), thread_type=threadType))
+
+	def getChats(self, n = 20):
+		threads = self.user.fetchThreadList()
+		threads += client.fetchThreadList(offset=n, limit=10)
+		print("Threads: {}".format(threads))
+
+	
+	def logout(self):
+		self.user.logout()
+
+
+
+
--- Python/main.py
+++ Python/main.py
@@ -2,4 +2,10 @@
 # @Author: Abhi
 # @Date:   2018-02-16 21:04:49
 # @Last Modified by:   Abhi
-# @Last Modified time: 2018-02-16 21:04:49
+# @Last Modified time: 2018-02-16 22:37:56
+
+from facebook import *
+
+if __name__ == "__main__":
+	user = Facebook("abhisoccerplayer@live.ca", "devfest")
+	user.getChats()
